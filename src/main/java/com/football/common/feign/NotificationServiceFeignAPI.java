cdbef51f011fd5e8f2b455ebb57058e3f873438b
Truong Nguyen <truongnguyen89@gmail.com>
2018-12-21 16:52:41 +0700

    Update NotificationServiceFeignAPI.java

--- src/main/java/com/football/common/feign/NotificationServiceFeignAPI.java
+++ src/main/java/com/football/common/feign/NotificationServiceFeignAPI.java
@@ -1,7 +1,12 @@
 package com.football.common.feign;
 
 import com.football.common.constant.Constant;
+import com.football.common.model.notification.NotificationQueue;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 /**
  * Created by IntelliJ IDEA.
@@ -12,5 +17,13 @@ import org.springframework.cloud.netflix.feign.FeignClient;
  */
 @FeignClient(Constant.FEIGN_CLIENT.SERVICE_NOTIFICATION)
 public interface NotificationServiceFeignAPI {
-    String URL_PATH_PARAM = "/param/";
+    String URL_PATH_NOTIFICATION = "/notification/";
+
+    @RequestMapping(path = URL_PATH_NOTIFICATION, method = POST)
+    NotificationQueue create(
+            @RequestParam(value = "title", required = true, defaultValue = "Tieu de thong bao") String title,
+            @RequestParam(value = "content", required = true, defaultValue = "Noi dung thong bao") String content,
+            @RequestParam(value = "type", required = true, defaultValue = "1") int type,
+            @RequestParam(value = "userId", required = true, defaultValue = "1") long userId
+    ) throws Exception;
 }
